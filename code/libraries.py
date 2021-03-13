cf77bb9ef3c677048f7161fa5e204ea36aa961ba
AngelVelazquezR <angel.velazquez@udem.edu>
2021-03-13 09:43:32 -0600

    Fonti

---
+++ code/libraries.py
@@ -0,0 +1,10 @@
+import cv2 as cv
+import numpy as np
+import glob
+import os
+import wget, zipfile, os
+import matplotlib.pyplot as plt
+import pytesseract
+import math
+
+print("Everything is ready!")
